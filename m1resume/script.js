35eaa5fb9e14071a1bd3b196f91c9ad5fe439480
SadiaMs <162547312+SadiaMs@users.noreply.github.com>
2024-11-05 17:58:10 +0500

    Update script.js

--- m1resume/script.js
+++ m1resume/script.js
@@ -1,7 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var toggleButton = document.getElementById('toggle-skills');
-    var skillsSection = document.getElementById('skills-section');
-    toggleButton.addEventListener('click', function () {
-        skillsSection.style.display = skillsSection.style.display === 'none' ? 'block' : 'none';
-    });
-});
+
